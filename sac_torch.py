b4a6e197aace7b83a8292c8a2ac09aa9ec0edad8
sinaebrahimi <3bin3m@gmail.com>
2024-03-18 11:47:34 +0000

    Reverting the reward back to the SSL-based

--- sac_torch.py
+++ sac_torch.py
@@ -10,7 +10,7 @@ class Agent():
         #     layer1_size = 500, layer2_size = 256, batch_size = 16, reward_scale = 1):
     def __init__(self, alpha, beta, n_actions, input_dims, tau = 0.001,
             layer1_size = 500, layer2_size = 256, batch_size = 64, reward_scale = 1):
-        self.gamma = 0.99 #0.8 in the TNSM22 paper # was 0.7 in orig code
+        self.gamma = 0.8 # 0.99 #0.8 in the TNSM22 paper # was 0.7 in orig code
         self.tau = tau # tau # 0.001 in the paper #was 0.5 in orig code
         self.pointer = 0
         self.max_size = 60000# 600000 #12000 # should be larger than T (episode number) # formerly 1000 in orig code
